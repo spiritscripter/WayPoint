f4e0c97ff7203f627051827019676e8e398610bd
Emmanuel Ashitey <eaashitey39@gmail.com>
2024-10-10 19:40:20 +0000

    Add initial project constants

---
+++ lib/core/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppConstants {
+  static const String appName = "WayPoint";
+  static const Color primaryColor = Color(0xFF6200EE);
+  static const Color secondaryColor = Color(0xFF03DAC6);
+  // Add more constants as needed
+}
