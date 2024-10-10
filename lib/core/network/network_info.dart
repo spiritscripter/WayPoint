5000df22e7917386b17bc855d44ce7d294137d16
Emmanuel Ashitey <eaashitey39@gmail.com>
2024-10-10 20:22:37 +0000

    Add NetworkInfo class for checking network connectivity

---
+++ lib/core/network/network_info.dart
@@ -0,0 +1,12 @@
+// import 'package:connectivity/connectivity.dart';
+
+// class NetworkInfo {
+//   final Connectivity connectivity;
+
+//   NetworkInfo(this.connectivity);
+
+//   Future<bool> get isConnected async {
+//     var result = await connectivity.checkConnectivity();
+//     return result != ConnectivityResult.none;
+//   }
+// }
